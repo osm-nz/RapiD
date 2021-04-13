04af70c58ade3ca2f8f0277e0c59394535ed576b
Kyle Hensel <k.y.l.e@outlook.co.nz>
2021-04-13 18:41:23 +1200

    make go-to-next use next nearest (fixes #2)

--- modules/ui/panels/history.js
+++ modules/ui/panels/history.js
@@ -2,16 +2,66 @@ import { t } from '../../core/localizer';
 import { modeRapidSelectFeatures } from '../../modes';
 import { utilKeybinding } from '../../util';
 
+const { sin, cos, sqrt, PI: π, atan2 } = Math;
+
+const R = 6371; // radius of the earth in km
+const K = π / 180; // marginal performance boost by pre-calculating this
+
+/** @param {number} deg */
+const deg2rad = (deg) => deg * K;
+
+/**
+ * returns the distance in metres between two coordinates
+ * @param {number} lat1
+ * @param {number} lng1
+ * @param {number} lat2
+ * @param {number} lng2
+ */
+function distanceBetween(lat1, lng1, lat2, lng2) {
+  const dLat = deg2rad(lat2 - lat1);
+  const dLon = deg2rad(lng2 - lng1);
+  const a =
+    sin(dLat / 2) * sin(dLat / 2) +
+    cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * sin(dLon / 2) * sin(dLon / 2);
+  const c = 2 * atan2(sqrt(a), sqrt(1 - a));
+  return 1000 * R * c;
+}
+
+/**
+ * @template T
+ * @param {T[]} list
+ * @param {number} ourLat
+ * @param {number} ourLng
+ * @returns {T}
+ */
+const findNearest = (list, ourLat, ourLng) => {
+    let closest;
+    let closestDistance;
+    for (const item of list) {
+        const [thisLng, thisLat] = item.geo || item.fromLoc;
+        const distance = distanceBetween(thisLat, thisLng, ourLat, ourLng);
+        if (!closest || distance < closestDistance) {
+            closest = item;
+            closestDistance = distance;
+        }
+    }
+    return closest;
+};
+
 /**
  * We've re-purposed this panel to be the status panel for addresses
  */
 
 export function uiPanelHistory(context) {
     function getNext() {
-        const data = window._dsState[window._mostRecentDsId];
-        let { 0: next, length } = Object.values(data).filter(x => x !== 'done');
+        // we can probably get this info from some context
+        const [/* zoom */, lat, lng ] = new URLSearchParams(location.hash).get('map').split('/').map(Number);
 
-        return { next, length };
+        const data = window._dsState[window._mostRecentDsId];
+        const list = Object.values(data).filter(x => x !== 'done');
+        const next = findNearest(list, lat, lng);
+
+        return { next, length: list.length };
     }
     function toNext() {
         const { next } = getNext();
